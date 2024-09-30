46c4efb2531e85e7539a5fa5cf3f025838cdc469
Raviteja582 <raviteja1028312@gmail.com>
2024-09-30 01:29:35 -0400

    Added event controllers for arithmetic operators

--- lib/main.dart
+++ lib/main.dart
@@ -4,122 +4,143 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+typedef ActionCallBack = void Function(Key key);
+typedef KeyCallBack = void Function(Key key);
+
+const Color primaryColor = Color(0xff50E3C2);
+const Color keypadColor = Color(0xff4A4A4A);
+
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // handler to control the text box
+  late TextEditingController _textEditingController;
+
+  // controllers for raising event for arthimatic operators
+  final Key plusOperator = const Key('plus');
+  final Key minusOperator = const Key('minus');
+  final Key multiplicationOperator = const Key('multiply');
+  final Key divisionOperator = const Key('divide');
+
+  var height;
+  var width;
+
+  @override
+  void initState() {
+    super.initState();
+    _textEditingController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    width = MediaQuery.of(context).size.width;
+    height = MediaQuery.of(context).size.height;
+    // debugPrint('Width :: $width and Height :: $height');
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment.bottomRight,
+            width: width,
+            height: (height / 100) * 20,
+            child: IgnorePointer(
+              child: TextField(
+                enabled: true,
+                autofocus: true,
+                controller: _textEditingController,
+                textAlign: TextAlign.right,
+                style: const TextStyle(
+                  fontFamily: 'Avenir',
+                  fontStyle: FontStyle.normal,
+                  color: Colors.white,
+                  fontSize: 60.0,
+                ),
+                decoration: const InputDecoration.collapsed(
+                    hintText: '0',
+                    hintStyle: TextStyle(color: Colors.white, fontSize: 60.0)),
+              ),
+            ),
+          ),
+          Row(
+            children: <Widget>[
+              bindOperatorEvent('+', plusOperator),
+              bindOperatorEvent('-', minusOperator),
+              bindOperatorEvent('x', multiplicationOperator),
+              bindOperatorEvent('/', divisionOperator)
+            ],
+          ),
+        ],
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  OperatorButton bindOperatorEvent(String name, Key key) {
+    return OperatorButton(
+      key: key,
+      actionName: name,
+      onTapped: (key) => {},
+      enabled: false,
+      padding:
+          height > 600 ? const EdgeInsets.all(10.0) : const EdgeInsets.all(0.0),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class OperatorButton extends StatelessWidget {
+  final Color defaultBackground = Colors.transparent;
+  final Color defaultForeground = primaryColor;
+  final Color changedBackground = primaryColor;
+  final Color changedForeground = Colors.white;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+  final String actionName;
+  final bool enabled;
+  final ActionCallBack onTapped;
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  // ignore: overridden_fields
+  final Key key;
+  final EdgeInsets padding;
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  const OperatorButton(
+      {required this.actionName,
+      required this.onTapped,
+      required this.enabled,
+      required this.key,
+      required this.padding})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Expanded(
+      child: Container(
+        alignment: Alignment.center,
+        padding: padding,
+        color: const Color(0xffF6F6F6),
+        child: GestureDetector(
+          onTap: () {
+            onTapped(key);
+          },
+          child: CircleAvatar(
+            backgroundColor: enabled ? changedBackground : defaultBackground,
+            radius: 30,
+            child: Text(
+              actionName,
+              style: TextStyle(
+                  color: enabled ? changedForeground : defaultForeground,
+                  fontSize: 40.0,
+                  fontFamily: 'Avenir',
+                  fontWeight: FontWeight.bold),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
